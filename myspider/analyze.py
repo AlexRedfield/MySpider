3b92b27976df551509b639659eb9d3f5655a3bf5
FlyRabbit <etenal@foxmail.com>
2017-01-10 15:10:04 +0800

    put regular expression into url.txt , can add param from last text

--- myspider/analyze.py
+++ myspider/analyze.py
@@ -3,17 +3,19 @@ import re
 
 class Analyze(object):
 
-    def __init__(self,method):
-        self.method=method
+    def __init__(self):
+        return
 
     def analyze(self, text, rule):
-        if self.method == 'display':
+        if rule == '':
             return self.analyze_display(text)
-        if self.method == 'regeexpr':
+        else:
             return self.analyze_regexpr(text, rule)
 
     def analyze_display(self, text):
-        return text
+        disp_list = []
+        disp_list.append(text)
+        return disp_list
 
     def analyze_regexpr(self, text, rule):
         return re.findall(rule, text)
